d995c8ef5b08bdf9da41ce1946225cb6f8a12ee0
Doning12 <connor.griesbaum@gmail.com>
2023-03-28 21:03:51 -0500

    add buttons to in index.html and added code to make some buttons disappear in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(document.getElementById("start"));
+document.getElementById("start").addEventListener('click', function () {
+    document.getElementById("start").style.visibility = 'hidden';
+});
