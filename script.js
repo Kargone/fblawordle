19d714a80a87e8c8f876c3bf25791244d8946fb3
Karson Martinson <karsonrty@gmail.com>
2023-03-28 21:10:12 -0500

    added some comments for connor

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(document.getElementById("start"));
+//this should be the longest part of the docu, maybe even a thousand lines long
 document.getElementById("start").addEventListener('click', function () {
     document.getElementById("start").style.visibility = 'hidden';
 });
